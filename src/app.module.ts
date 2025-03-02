51d1fcb25cb4a3a6fb38029eabc19a61709c55b7
chaton444 <chaton444@outlook.com>
2025-03-02 16:10:08 -0600

    agregar modulo de bd para el posgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ImageService } from './image/image.service';
-import { ImageController } from './image/image.controller';
-import { Image } from './entity/image.entity';
+import { ImageModule } from './image/image.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image])],
-  controllers: [ImageController],
-  providers: [ImageService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'image_user',//credenciales de posgresql
+      password: 'password123',//credenciales de posgresql
+      database: 'image_db',//nombre de la bd hecha en posgresql revisar el archivo de como crear tablas en posgresql
+      autoLoadEntities: true,
+      synchronize: true, // No usar en producción
+    }),
+    ImageModule,
+  ],
 })
-export class ImageModule {}
+export class AppModule {}
