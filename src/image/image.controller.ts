a38fd86324d38bea0adf01cf05bc58bcd6a7c0a9
chaton444 <chaton444@outlook.com>
2025-05-07 20:27:06 -0600

    ww

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -45,7 +45,7 @@ export class ImageController {
       id: img.id,
       filename: img.filename,
       plate: 'DESCONOCIDA', // Puedes personalizar si guardas placas
-      image: `https://3.23.102.253/:3000/uploads/${img.filename}`,
+      image: `https://3.23.102.253:3000/uploads/${img.filename}`,
     }));
   }
   
