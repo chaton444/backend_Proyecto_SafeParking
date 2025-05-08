e614f12b8c2b4e2ad8e3e081d8a9d14c911c5c1f
chaton444 <chaton444@outlook.com>
2025-05-07 20:45:32 -0600

    soluciones de problemas de carga de imagenes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-   // Habilitar CORS y no tener problemas con las peticiones 
-   app.enableCors({
-    origin: '*', 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // metodos que se van a usar en un futuro o actualmente para lo clasico de modificarciones de bd
-    credentials: true, // para permitir las credenciales para la seguridad
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); // 👈 importante
+
+  // Servir archivos estáticos desde la carpeta "uploads"
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
